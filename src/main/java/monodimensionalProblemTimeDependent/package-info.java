339b9eb4e95adbfb64f6b0e595594f640c31c723
niccolotubini <tubini.niccolo@gmail.com>
2018-04-30 17:39:03 +0200

    package info

--- src/main/java/monodimensionalProblemTimeDependent/package-info.java
+++ src/main/java/monodimensionalProblemTimeDependent/package-info.java
@@ -4,6 +4,9 @@
 /**
  * This is an attempt to to store results of a 1D pde integration in NetCDF by using a buffer to store variables until the computation is going on.
  * Then a .py code is used to read data a plot them.
+ * 
+ * BufferWriter and WriteMain are two classes created to learn how to use NetCDF
+ * 
  * @author Niccolo` Tubini
  *
  */
