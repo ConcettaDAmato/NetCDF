04e8f6e39b1ae1c1132025a6f1b444d14ab727e4
niccolotubini <tubini.niccolo@gmail.com>
2018-04-30 17:37:06 +0200

    package info

---
+++ src/main/java/bufferWriter/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author Niccolo` Tubini
+ * This package collects buffers to store outputs during the simulation and save them on a file only once the simulation is finished.
+ * This will become an OMS component, and it is independent on the format of the output files.
+ * 
+ * It will be necessary to write this component in the most generic way!
+ *
+ */
+package bufferWriter;
