98fb808a4f6e4353fd5ec31307011516408c86e3
niccolotubini <tubini.niccolo@gmail.com>
2020-10-22 17:05:06 +0200

    Buffer for the model Richards1D

--- src/main/java/bufferWriter/RichardsBuffer1D.java
+++ src/main/java/bufferWriter/RichardsBuffer1D.java
@@ -35,7 +35,7 @@ import oms3.annotations.*;
 
 public class RichardsBuffer1D {
 	
-	@Description("Varible to store")
+	@Description("Variable to store")
 	@In 
 	@Unit ("-")
 	public ArrayList<double[]> inputVariable;
@@ -45,95 +45,90 @@ public class RichardsBuffer1D {
 	@Unit ("YYYY-MM-DD HH:mm")
 	public String inputDate;
 	
-	@Description("Spatial coordinate: is the position of the centroids ")
+	@Description("Boolean value controlling the buffer component")
 	@In 
-	@Unit ("m")
-	public double[] inputSpatialCoordinate;
+	@Unit ("-")
+	public boolean doProcessBuffer;
 	
-	@Description("Dual spatial coordinate: is the position of volumes' interfaces ")
 	@In 
-	@Unit ("m")
-	public double[] inputDualSpatialCoordinate;
-	
+	public int writeFrequency = 1;
 	
 	@Description()
 	@Out
 	@Unit ()
 	public LinkedHashMap<String,ArrayList<double[]>> myVariable = new LinkedHashMap<String,ArrayList<double[]>>(); // consider the opportunity to save varibale as float instead of double
 	
-	@Description()
-	@Out
-	@Unit ()
-	public double[] mySpatialCoordinate;
-	
-	@Description()
-	@Out
-	@Unit ()
-	public double[] myDualSpatialCoordinate;
 	
 	@Description("")
-	int step=0;
+	private int step=0;
 	
-	ArrayList<double[]> tempVariable;
+	private ArrayList<double[]> tempVariable;
 	
 	
 	
 	@Execute
 	public void solve() {
-		//System.out.println("Buffer1D step:" + step);
+
 		if(step==0){
 			
-		mySpatialCoordinate = inputSpatialCoordinate;
-		myDualSpatialCoordinate = inputDualSpatialCoordinate;
-		
-		tempVariable = new ArrayList<double[]>();
-		//System.out.println(mySpatialCoordinate.toString());
+			tempVariable = new ArrayList<double[]>();
 		
 		}
 		
-		// water suction values
-		tempVariable.add(inputVariable.get(0).clone());
-		
-		// thetas
-		tempVariable.add(inputVariable.get(1).clone());
-		
-		// psiIC
-		tempVariable.add(inputVariable.get(2).clone());
-		
-		// Darcy velocities
-		tempVariable.add(inputVariable.get(3).clone());
-		
-		// Darcy velocities due to capillary gradient
-		tempVariable.add(inputVariable.get(4).clone());
-		
-		// Darcy velocities due to gravity gradient
-		tempVariable.add(inputVariable.get(5).clone());
-		
-		// pore velocities 
-		tempVariable.add(inputVariable.get(6).clone());
-		
-		// celerities
-		tempVariable.add(inputVariable.get(7).clone());
-		
-		// kinematic ratio
-		tempVariable.add(inputVariable.get(8).clone());
-		
-		// errorVolume
-		tempVariable.add(inputVariable.get(9).clone());
-		
-		// top boundary condition value
-		tempVariable.add(inputVariable.get(10).clone());
-		
-		// bottom boundary condition value
-		tempVariable.add(inputVariable.get(11).clone());
-		
-		// surface run-off
-		tempVariable.add(inputVariable.get(12).clone());
+		if( ((step-1)%writeFrequency) == 0 || step == 1) {
+//			System.out.println("Buffer1D clear");
 
-		myVariable.put(inputDate,(ArrayList<double[]>) tempVariable.clone());
-		//System.out.println(myVariable.size() +"       "+ myVariable.keySet());
-		//System.out.println(myVariable.toString());
-		tempVariable.clear();
+			myVariable.clear();
+
+		}
+		
+		if(doProcessBuffer== true) {
+			// water suction values
+			tempVariable.add(inputVariable.get(0).clone());
+
+			// thetas
+			tempVariable.add(inputVariable.get(1).clone());
+			
+			// water volume
+			tempVariable.add(inputVariable.get(2).clone());
+
+			// Darcy velocities
+			tempVariable.add(inputVariable.get(3).clone());
+
+			// Darcy velocities due to capillary gradient
+			tempVariable.add(inputVariable.get(4).clone());
+
+			// Darcy velocities due to gravity gradient
+			tempVariable.add(inputVariable.get(5).clone());
+
+			// pore velocities 
+			tempVariable.add(inputVariable.get(6).clone());
+
+			// celerities
+			tempVariable.add(inputVariable.get(7).clone());
+
+			// kinematic ratio
+			tempVariable.add(inputVariable.get(8).clone());
+
+			//ETs i.e. transpired stressed water
+			tempVariable.add(inputVariable.get(9).clone());
+			
+			// errorVolume
+			tempVariable.add(inputVariable.get(10).clone());
+
+			// top boundary condition value
+			tempVariable.add(inputVariable.get(11).clone());
+
+			// bottom boundary condition value
+			tempVariable.add(inputVariable.get(12).clone());
+
+			// surface run-off
+			tempVariable.add(inputVariable.get(13).clone());
+
+			myVariable.put(inputDate,(ArrayList<double[]>) tempVariable.clone());
+
+			tempVariable.clear();
+		}
 		step++;
 		
 	}
